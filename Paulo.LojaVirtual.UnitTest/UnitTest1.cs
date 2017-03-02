54b9918b80450bc2252ef2bdea0f705649cca217
Gabriel <Gabriel@GABRIEL-PC>
2017-03-02 15:00:23 -0300

    Add project files.

---
+++ Paulo.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Paulo.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestPaulo
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
